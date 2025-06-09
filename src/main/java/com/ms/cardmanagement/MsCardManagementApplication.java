ec515953081b268cdcb38f708b3a72453a25f571
José Vinicius Alves dos Santos <jose.vinicius7@hotmail.com>
2025-06-09 10:51:39 -0300

    feat: projeto inicial com estrutura base do microserviço de cartões

---
+++ src/main/java/com/ms/cardmanagement/MsCardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ms.cardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCardManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCardManagementApplication.class, args);
+	}
+
+}
